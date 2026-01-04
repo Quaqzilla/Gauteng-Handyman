705557c6aa41d1d8b2dc522151af1fb83ba4084d
Quaqzilla <bbrigett207@gmail.com>
2026-01-04 15:56:30 +0200

    gh-pages initialization

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/Gauteng-Handyman",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
